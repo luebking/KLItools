ccc9d0b09d6c88899dea17b058d6fb142faa5755
Thomas Lübking <thomas.luebking@gmail.com>
2014-07-22 10:58:47 +0200

    fix lib paths for distros with funny installation paths of Qt and sane for KDE (ie. Ubuntu)

--- make_kconfig.sh
+++ make_kconfig.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 if ( [ ! -e kconfig ] || [ kconfig.cpp -nt kconfig ] ); then
+    for lib in $(ldd `which kde4-config` | sed '/\(libkdecore\.so\|libQtCore\.so\)/!d; s/^.* => \([^ ]*\) .*/\1/g'); do
+        LIB_PATH="${LIB_PATH} -L`dirname $lib`"
+    done
     g++ `pkg-config --libs --cflags QtCore` \
-        -I`kde4-config --path include | sed 's%:%KDE -I%g; s%$%KDE%g'` \
-        -L`kde4-config --path lib | sed 's/:/ -L/g'` -lkdecore -o kconfig kconfig.cpp
+        -I`kde4-config --path include | sed 's%:%KDE -I%g; s%$%KDE%g'` $LIB_PATH  -lkdecore -o kconfig kconfig.cpp
 fi
 if [ "$1" = "install" ]; then
     install -v kconfig "`kde4-config --prefix`/bin/"
--- make_kwindowsystem.sh
+++ make_kwindowsystem.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 if ( [ ! -e kwindowsystem ] || [ kwindowsystem.cpp -nt kwindowsystem ] ); then
+    for lib in $(ldd `which kde4-config` | sed '/\(libkdecore\.so\|libQtCore\.so\)/!d; s/^.* => \([^ ]*\) .*/\1/g'); do
+        LIB_PATH="${LIB_PATH} -L`dirname $lib`"
+    done
+    echo $LIB_PATH
     g++ `pkg-config --libs --cflags QtGui` -lX11 \
-        -I`kde4-config --path include | sed 's%:%KDE -I%g; s%$%KDE%g'` \
-        -L`kde4-config --path lib | sed 's/:/ -L/g'` -lkdeui -o kwindowsystem kwindowsystem.cpp
+        -I`kde4-config --path include | sed 's%:%KDE -I%g; s%$%KDE%g'` $LIB_PATH -lkdeui -o kwindowsystem kwindowsystem.cpp
 fi
 if [ "$1" = "install" ]; then
     install -v kwindowsystem "`kde4-config --prefix`/bin/"
