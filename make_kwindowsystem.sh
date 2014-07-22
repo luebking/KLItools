73cb221dd9ae78c266c0543e3e81c61e81cfb8cf
Thomas Lübking <thomas.luebking@gmail.com>
2014-07-22 12:12:18 +0200

    no reason to print this

--- make_kwindowsystem.sh
+++ make_kwindowsystem.sh
@@ -3,7 +3,6 @@ if ( [ ! -e kwindowsystem ] || [ kwindowsystem.cpp -nt kwindowsystem ] ); then
     for lib in $(ldd `which kde4-config` | sed '/\(libkdecore\.so\|libQtCore\.so\)/!d; s/^.* => \([^ ]*\) .*/\1/g'); do
         LIB_PATH="${LIB_PATH} -L`dirname $lib`"
     done
-    echo $LIB_PATH
     g++ `pkg-config --libs --cflags QtGui` -lX11 \
         -I`kde4-config --path include | sed 's%:%KDE -I%g; s%$%KDE%g'` $LIB_PATH -lkdeui -o kwindowsystem kwindowsystem.cpp
 fi
